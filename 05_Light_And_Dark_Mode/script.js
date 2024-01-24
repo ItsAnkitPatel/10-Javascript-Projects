ab6c2331a4ce8685218b975104bf118a0ab0f120
ItsAnkitPatel <itsakppatel@gmail.com>
2024-01-24 23:15:35 +0530

    Project - 5: LocalStorage added to keep the theme persist on every load

--- 05_Light_And_Dark_Mode/script.js
+++ 05_Light_And_Dark_Mode/script.js
@@ -34,10 +34,24 @@ function lightMode() {
 function switchTheme(event) {
   if (event.target.checked) {
     document.documentElement.setAttribute("data-theme", "dark");
+    localStorage.setItem("theme","dark")
     darkMode();
-  } else {
+} else {
     document.documentElement.setAttribute("data-theme", "light");
+    localStorage.setItem("theme","light")
     lightMode();
   }
 }
 toggleSwitch.addEventListener("change", switchTheme);
+
+// Check Local Storage For theme
+const currentTheme = localStorage.getItem('theme')
+
+if(currentTheme){
+   document.documentElement.setAttribute('data-theme',currentTheme)
+   
+   if(currentTheme === 'dark'){
+    toggleSwitch.checked ='true'
+    darkMode()
+   }
+}
